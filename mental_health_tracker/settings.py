11b87d22c8e7f85023dbb7ede4d3cb950c9df5bc
denisfilab <daanish.inayat@ui.ac.id>
2024-09-11 14:23:00 +0700

    secret key,

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -54,8 +56,8 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+      'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://daanish-inayat-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://daanish-inayat-mentalhealthtracker.pbp.cs.ui.ac.id/"]
